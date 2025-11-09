4870a9d5fe112295f61c5f19d9970d3602bb368a
ChrisMicMad <123459412+ChrisMicMad@users.noreply.github.com>
2025-11-09 23:39:16 +0100

    dataset updates...

--- docker-shell_8.sh
+++ docker-shell_8.sh
@@ -24,10 +24,10 @@ fi
 
 echo "Running container"
 docker run --rm --name data-version-cli -ti \
---cap-add SYS_ADMIN \
+--device /dev/fuse \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
 
-# does not work - bucket not visible in dvc
+# this works
